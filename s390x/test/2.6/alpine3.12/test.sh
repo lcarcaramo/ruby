5e139c646445adff78d0d6d7594e8cda53ef4dff
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-11-03 06:50:54 -0500

    Add test

--- s390x/test/2.6/alpine3.12/test.sh
+++ s390x/test/2.6/alpine3.12/test.sh
@@ -1,3 +1,55 @@
+#!/bin/bash
+
 set -e
 
-echo -e "No tests yet."
+export ANSI_YELLOW_BOLD="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_YELLOW_BACKGROUND="\e[1;7;33m"
+export ANSI_GREEN_BACKGROUND="\e[1;7;32m"
+export ANSI_CYAN="\e[36m"
+export ANSI_RESET="\e[0m"
+export DOCKERFILE_TOP="**************************************** DOCKERFILE ******************************************"
+export DOCKERFILE_BOTTOM="**********************************************************************************************"
+export TEST_SUITE_START="**************************************** SMOKE TESTS *****************************************"
+export TEST_SUITE_END="************************************** TEST SUCCESSFUL ***************************************"
+
+# Pass in path to folder where Dockerfile lives
+print_dockerfile () {
+        echo -e "\n$ANSI_CYAN$DOCKERFILE_TOP\n$(<$1/Dockerfile)\n$ANSI_CYAN$DOCKERFILE_BOTTOM $ANSI_RESET\n"
+}
+
+# Pass in test case message
+print_test_case () {
+        echo -e "\n$ANSI_YELLOW_BOLD$1 $ANSI_RESET"
+}
+
+print_success () {
+        echo -e "\n$ANSI_GREEN$1 $ANSI_RESET \n"
+
+}
+
+# Pass in path to folder where Dockerfile lives
+build () {
+        print_dockerfile $1
+        docker build -q -t $1 $1
+}
+
+cleanup () {
+        docker rmi $1
+}
+
+suite_start () {
+        echo -e "\n$ANSI_YELLOW_BACKGROUND$TEST_SUITE_START$ANSI_RESET \n"
+}
+
+suite_end () {
+        echo -e "\n$ANSI_GREEN_BACKGROUND$TEST_SUITE_END$ANSI_RESET \n"
+}
+
+
+suite_start
+        print_test_case "It can run a Ruby app:"
+                build "runs-ruby-apps" 
+                docker run --rm --name runs-ruby-apps "runs-ruby-apps"
+                cleanup "runs-ruby-apps"
+suite_end
